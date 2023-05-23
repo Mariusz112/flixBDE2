0a0e4e7c3e3c92f774941cde57a915fa1fcbe374
Mariusz112 <maniek11744@gmail.com>
2023-05-23 20:58:03 +0200

    log works, register w8 for back to test

--- src/app/registration/registration.component.ts
+++ src/app/registration/registration.component.ts
@@ -1,29 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-registration',
   templateUrl: './registration.component.html',
   styleUrls: ['./registration.component.css']
 })
-export class RegistrationComponent {
+export class RegistrationComponent implements OnInit {
+  registrationForm!: FormGroup;
   email: string = '';
   password: string = '';
   confirmPassword: string = '';
 
-  registerUser() {
+  constructor(private formBuilder: FormBuilder, private http: HttpClient) {}
 
-    if (this.password !== this.confirmPassword) {
-      console.log('Passwords do not match');
+  ngOnInit() {
+    this.registrationForm = this.formBuilder.group({
+      email: ['', Validators.required],
+      password: ['', Validators.required],
+      confirmPassword: ['', Validators.required]
+    });
+  }
+
+  registerUser() {
+    if (this.registrationForm.invalid) {
+      // Handle form validation errors
       return;
     }
 
-    const registrationPayload = {
-      email: this.email,
-      password: this.password
+    const email = this.registrationForm.get('email')!.value;
+    const password = this.registrationForm.get('password')!.value;
+    const confirmPassword = this.registrationForm.get('confirmPassword')!.value;
+
+    const registrationData = {
+      email: email,
+      password: password,
+      confirmPassword: confirmPassword
     };
 
+    this.http.post('http://localhost:8080/api/authentication/signup', registrationData).subscribe(
+      (response) => {
+        console.log('Registration response:', response);
 
+      },
+      (error) => {
+        console.log('Registration error:', error);
 
-    console.log('Registering user:', this.email, this.password, this.confirmPassword);
+      }
+    );
   }
 }

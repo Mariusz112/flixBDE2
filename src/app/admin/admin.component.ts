daa8bf0f211f2b0280b19d7d43eecc6b790c2af3
Mariusz112 <maniek11744@gmail.com>
2023-06-01 22:48:39 +0200

    update do up filmow

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -23,21 +23,33 @@ export class AdminComponent {
 
   addFilm() {
     const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
-
-    this.http.post('http://localhost:8080/api/film', this.newFilm, { headers }).subscribe(
-      () => {
+  
+    const requestBody = {
+      title: 'implement extensible schemas3',
+      duration: '7422',
+      description: 'Wonder each design moment quickly total turn. Tend claim listen short. Form half ok computer set.',
+      releaseDate: '2012-04-23',
+      poster: 'https://fwcdn.pl/fpo/10/45/1045/7563795.2.jpg',
+      director: 'Caroline Krueger',
+      actorsCast: ['Actor1', 'Actor2'],
+      path: './',
+      genreTag: ['Romance', 'Comedy']
+    };
+  
+    this.http.post('http://localhost:8080/api/film', JSON.stringify(requestBody), { headers }).subscribe(
+      (response) => {
         console.log('Film added successfully!');
         // Reset the form
         this.newFilm = {
           title: '',
-          duration: 0,
-          description: '',
-          releaseDate: '',
-          poster: '',
           director: '',
+          duration: 0,
+          releaseDate: '',
+          description: '',
+          poster: '',
           actorsCast: [],
-          path: '',
-          genreTag: []
+          genreTag: [],
+          path: ''
         };
       },
       (error) => {
